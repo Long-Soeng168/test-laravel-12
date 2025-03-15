740947f8921f692ce252b1890fb6f2c7f7d80b27
Long-Soeng168 <generalpurpose167@gmail.com>
2025-03-15 15:56:34 +0700

    update

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -14,11 +14,16 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
     return (
         <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div className="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
-                <div className="absolute inset-0 bg-zinc-900" />
+                <div className="absolute inset-0 bg-zinc-900">
+                    <div className="flex h-full items-center justify-center">
+                        <AppLogoIcon className="mr-2 size-40 fill-current text-white" />
+                    </div>
+                </div>
                 <Link href={route('home')} className="relative z-20 flex items-center text-lg font-medium">
                     <AppLogoIcon className="mr-2 size-8 fill-current text-white" />
                     {name}
                 </Link>
+                <AppLogoIcon className="mr-2 size-8 fill-current text-white" />
                 {quote && (
                     <div className="relative z-20 mt-auto">
                         <blockquote className="space-y-2">
