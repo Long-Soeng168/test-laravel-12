e5a20d03043b05b3c9c12711a3712d1675037fce
Long-Soeng168 <generalpurpose167@gmail.com>
2025-03-26 19:11:28 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return 'Hello World';
+    return Inertia::render('LaravelDefaultPage');
 })->name('home');
 
 // Route::get('/', function () {
