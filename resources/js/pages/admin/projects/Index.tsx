ffe62dd3523c677cdd2d8edd41bae4c239ee7b28
Long-Soeng168 <generalpurpose167@gmail.com>
2025-03-27 10:37:00 +0700

    update

--- resources/js/pages/admin/projects/Index.tsx
+++ resources/js/pages/admin/projects/Index.tsx
@@ -1,11 +1,11 @@
 import { MyPagination } from '@/components/my-pagination';
+import { MyRefreshButton } from '@/components/my-refresh-button';
+import { MySearchTableData } from '@/components/my-search-table-data';
 import AppLayout from '@/layouts/app-layout';
+import { BreadcrumbItem } from '@/types';
 import AddNewButton from './components/add-new-button';
 import { MyFilterButton } from './components/my-filter-button';
 import MyTableData from './components/my-table-data';
-import { MyRefreshButton } from '@/components/my-refresh-button';
-import { BreadcrumbItem } from '@/types';
-import { MySearchTableData } from '@/components/my-search-table-data';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -16,13 +16,14 @@ const breadcrumbs: BreadcrumbItem[] = [
 const Index = () => {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            <div className="flex max-w-[100vw] flex-wrap items-center justify-between gap-2">
-                <div className="flex max-lg:w-full lg:flex-1 items-center justify-start gap-2 max-w-[100vw] flex-wrap">
+            <div className="flex max-w-[100vw] flex-wrap items-center justify-end gap-2">
+                <div className="flex max-w-[100vw] flex-wrap items-center justify-start gap-2 max-lg:w-full lg:flex-1">
                     <MySearchTableData />
                     <MyFilterButton />
                     <MyRefreshButton />
+                    <span className="flex-1"></span>
+                    <AddNewButton />
                 </div>
-                <AddNewButton />
             </div>
             <div className="h-2" />
             <MyTableData />
